06c8fcb96cb47eaef345cd32e61cb6358647beb4
mohini-442 <mohinikarwasra598@gmail.com>
2024-06-05 11:23:51 -1200

    commit2

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-// app.post("/test", (req, res) => {
-//   var name = req.body.name;
-//   var pswd = req.body.pswd;
-//   var users = {
-//     name,
-//     pswd,
-//   };
-//   user.push(users);
-//   res.json({
-//     message: `my name is ${name} and id ${pswd}`,
-//     user,
-//   });
-// });
-
 var user = [];
 
 app.post("/signup", (req, res) => {
@@ -26,11 +12,17 @@ app.post("/signup", (req, res) => {
   var pswd = req.body.pswd;
   var city = req.body.city;
   var age = req.body.age;
+  var pin = red.body.pin;
+  var mail = red.body.mail;
+
   var users = {
     name,
     pswd,
     city,
     age,
+    mail,
+    pin,
+    purchased: [],
   };
   user.push(users);
   res.json({
@@ -38,6 +30,8 @@ app.post("/signup", (req, res) => {
     pswd,
     city,
     age,
+    mail,
+    pin,
   });
 });
 
@@ -75,6 +69,95 @@ app.post("/changepass", (req, res) => {
       }
     }
   }
+});
+
+var seller = [];
+
+app.post("/sellersignup", (req, res) => {
+  var name = req.body.name;
+  var pswd = req.body.pswd;
+  var city = req.body.city;
+  var age = req.body.age;
+  var users = {
+    name,
+    pswd,
+    city,
+    age,
+  };
+  seller.push(users);
+  res.json({
+    name,
+    pswd,
+    city,
+    age,
+  });
+});
+
+app.post("/sellerlogin", (req, res) => {
+  const name = req.body.name;
+  const pswd = req.body.pswd;
+  for (let i = 0; i < seller.length; i++) {
+    if (seller[i].name === name) {
+      if (seller[i].pswd === pswd) {
+        res.json({
+          message: "login success",
+          city: seller[i].city,
+          age: seller[i].age,
+        });
+        return;
+      }
+    } else {
+      res.json("Login failed");
+    }
+  }
+  res.json({ message: "user not found" });
+});
+
+app.post("/addproduct", (req, res) => {
+  var name = req.body.name;
+  var price = req.body.price;
+  var img = req.body.img;
+  var description = req.body.description;
+  var id = req.body.id;
+  var users = {
+    name,
+    price,
+    description,
+    id,
+    img,
+  };
+  seller.push(users);
+  res.json({
+    name,
+    price,
+    description,
+    id,
+    img,
+  });
+});
+
+app.post("/purchased", (req, res) => {
+  const name = req.body.name;
+  const pswd = req.body.pswd;
+  const itemid = req.body.itemid;
+  for (let i = 0; i < products.length; i++) {
+    if (user[i].name === name) {
+      if (user[i].pswd === pswd)
+        for (let g = 0; g < products.length; g++) {
+          if (products[g].name === itemid) {
+            user[g].purchased.push(products[g]);
+            res.json(user[g]);
+          }
+        }
+    } else {
+      res.json("user not found");
+    }
+  }
+  res.json({ massage: "seller not found" });
+});
+
+app.get("/getproduct", (req, res) => {
+  res.json(seller);
 });
 
 app.listen(port, () => {
